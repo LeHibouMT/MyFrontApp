45e69d5951fb00f32d7799b635389f41787386aa
LeHibouMT <nickylarson6665@gmail.com>
2024-12-14 03:06:52 +0100

    Footer and completed Settings style
    - Modified Footer
    - Modified .less files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,18 @@
+import useTranslation from "hooks/useTranslation";
+
 /**
  * The Footer component is visible at the bottom of all pages.
  * @returns The component.
  */
 const Footer: React.FC = () => {
-  return <footer className="footer">Footer</footer>;
+  const ts = useTranslation();
+  return (
+    <footer className="footer">
+      <span>{ts.footerSignatureText}</span>
+      <span>{ts.footerRightsText}</span>
+      <span>{ts.footerCookiesText}</span>
+    </footer>
+  );
 };
 
 export default Footer;
